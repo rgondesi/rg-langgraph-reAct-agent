b9224d69e59d19bafd85889040ccf3e51bf276ee
Rgondesi <raveendhar.gondesi@gmail.com>
2025-07-31 14:52:39 -0500

    working version of reflexion- no errors

--- main.py
+++ main.py
@@ -1,39 +1,49 @@
-from tkinter import END
 from dotenv import load_dotenv
-import os
-from langchain_core.messages import HumanMessage
-from langgraph.graph import MessagesState, StateGraph
-from nodes import run_agent_reasoning, tool_node
+from typing import List
 
 load_dotenv()
 
-AGENT_REASON="agent_reason"
-ACT="act"
-END="__end__"
-LAST=-1
+
+from langchain_core.messages import BaseMessage, ToolMessage
+
+from langgraph.graph import MessageGraph, END
+from langchain_openai import ChatOpenAI
+from chains import revisor, first_responder
+from tool_executor import execute_tools
+
+MAX_ITERATIONS=2
 
 
-def should_continue(state:MessagesState) -> str:
-    if not state["messages"][LAST].tool_calls:
+builder = MessageGraph()
+builder.add_node("draft", first_responder)
+builder.add_node("execute_tools", execute_tools)
+builder.add_node("revise", revisor)
+builder.add_edge("draft", "execute_tools")
+builder.add_edge("execute_tools", "revise")
+
+
+def event_loop(state: List[BaseMessage]) -> str:
+    count_tool_visits = sum(isinstance(item, ToolMessage) for item in state)
+    num_iterations = count_tool_visits
+    if num_iterations > MAX_ITERATIONS:
         return END
-    return ACT
+    return "execute_tools"
 
-flow = StateGraph(MessagesState)
-flow.add_node(AGENT_REASON, run_agent_reasoning)
-flow.set_entry_point(AGENT_REASON)
-flow.add_node(ACT,tool_node)
-flow.add_conditional_edges(AGENT_REASON, should_continue, {
-    END:END,
-    ACT:ACT
-})
 
-flow.add_edge(ACT, AGENT_REASON)
+builder.add_conditional_edges("revise", event_loop)
+builder.set_entry_point("draft")
+graph = builder.compile()
 
-app= flow.compile()
-app.get_graph().draw_mermaid_png(output_file_path="flow.png")
+print(graph.get_graph().draw_ascii())
+
+graph.get_graph().draw_mermaid_png(output_file_path="reflexion_flow.png")
 
 
 if __name__=="__main__":
-    print("Hello ReAct Message!")
-    res = app.invoke({"messages": [HumanMessage(content="What is the weather temperature in Richmond, texas? List it and triple it")]})
-    print(res["messages"][LAST].content)
+    print("Hello Reflexion")
+    res = graph.invoke(
+        "Write about AI-Powered SOC / autonomous soc  problem domain, list startups that do that and raised capital."
+    )
+    print(res[-1].tool_calls[0]["args"]["answer"])
+    print(res)
+
